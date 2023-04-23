af017799099e81ea1dc064dea29a91255b1cedfe
LukasKrah <l.krahbichler05@gmail.com>
2023-04-23 23:30:33 +0200

    Hotkey
    Took 7 minutes

--- main.py
+++ main.py
@@ -7,50 +7,44 @@ Author: Lukas Krahbichler
 """
 
 from concurrent.futures import ThreadPoolExecutor
-from pynput import keyboard
 from os import kill, getpid
 from time import sleep
+import keyboard
 
 from screen_service import InputService, OutputService
 from algorithm import AlgorithmService
 from cv_analyze import CompareService
 
 
-def on_press(_key):
-    ...
+def start():
+    pads = InputService.pda_single()
+    io_pads = InputService.pda_io()
 
+    styles = CompareService.compare_pads(pads)
+    io = CompareService.compare_io(io_pads)
+    clicks = AlgorithmService.path_finder(pads, styles, io)
 
-def on_release(key):
-    if str(key) == "'o'":
-        pads = InputService.pda_single()
-        io_pads = InputService.pda_io()
-
-        styles = CompareService.compare_pads(pads)
-        io = CompareService.compare_io(io_pads)
-        clicks = AlgorithmService.path_finder(pads, styles, io)
-
-        OutputService.pad_clicks(clicks)
-
-    elif str(key) == "'c'":
-        kill(getpid(), 9)
-
-
-def key_input():
-    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-        listener.join()
+    OutputService.pad_clicks(clicks)
 
 
 def test():
     import autoit
 
-    sleep(2)
-    while True:
-        autoit.mouse_click("left", speed=0)
-        sleep(0.1)
-        on_release("'o'")
-        sleep(3)
+    def inner_function():
+        sleep(2)
+        while True:
+            autoit.mouse_click("left", speed=0)
+            sleep(0.1)
+            start()
+            sleep(3)
+
+    tp = ThreadPoolExecutor(max_workers=1)
+    tp.submit(inner_function)
 
 
 if __name__ == '__main__':
-    tp = ThreadPoolExecutor(max_workers=1)
-    tp.submit(key_input)
+    keyboard.add_hotkey("ctrl+x", start)
+    keyboard.add_hotkey("ctrl+v", test)
+    keyboard.add_hotkey("ctrl+c", lambda: kill(getpid(), 9))
+
+    keyboard.wait()
